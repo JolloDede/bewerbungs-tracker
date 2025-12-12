7c4abcca2e55903a572096d7bb1d7fffe5517d4d
Dennis Lee nix <dennis.lee@gmx.ch>
2025-12-12 18:09:43 +0100

    refactor: only setup table when db not exists

--- database/database.go
+++ database/database.go
@@ -1,55 +1,67 @@
 package database
 
-import "database/sql"
+import (
+	"database/sql"
+	"os"
+)
 
 var DB *sql.DB
 
 func Init() error {
-	var err error
-	DB, err = sql.Open("sqlite3", "file:tmp/test.db")
+	dbExists := true
+	const dbFile = "tmp/test.db"
+	_, err := os.Stat(dbFile)
+
+	if err != nil {
+		dbExists = false
+	}
+
+	DB, err = sql.Open("sqlite3", "file:"+dbFile)
 	if err != nil {
 		return err
 	}
 
-	err = setupTables()
+	if !dbExists {
+		err = setupTables()
 
-	if err != nil {
-		return err
+		if err != nil {
+			return err
+		}
 	}
 
 	return nil
 }
 
 func setupTables() error {
-	// _, err := DB.Exec(`
-	// DROP TABLE IF EXISTS contact;
-	// DROP TABLE IF EXISTS firma;
-	// `)
+	_, err := DB.Exec(`
+	DROP TABLE IF EXISTS contact;
+	DROP TABLE IF EXISTS firma;
+	`)
 
-	// if err != nil {
-	// 	return err
-	// }
+	if err != nil {
+		return err
+	}
 
-	// _, err = DB.Exec(`
-	// CREATE TABLE firma (
-	// 	id			TEXT PRIMARY KEY NOT NULL, -- UUID
-	// 	name		TEXT NOT NULL,
-	// 	urls		TEXT,
-	// 	created_at	TEXT NOT NULL
-	// );
+	_, err = DB.Exec(`
+	CREATE TABLE firma (
+		id			TEXT PRIMARY KEY NOT NULL, -- UUID
+		name		TEXT NOT NULL,
+		urls		TEXT,
+		created_at	TEXT NOT NULL
+	);
 
-	// CREATE TABLE contact (
-	// 	id			TEXT PRIMARY KEY NOT NULL, -- UUID
-	// 	date		TEXT NOT NULL,
-	// 	type		TEXT NOT NULL,
-	// 	fk_firma	TEXT NOT NULL,
-	// 	FOREIGN KEY(fk_firma) REFERENCES firma(id)
-	// );
-	// `)
+	CREATE TABLE contact (
+		id			TEXT PRIMARY KEY NOT NULL, -- UUID
+		date		TEXT NOT NULL,
+		type		TEXT NOT NULL,
+		fk_firma	TEXT NOT NULL,
+		FOREIGN KEY(fk_firma) REFERENCES firma(id)
+	);
+	`)
 
-	// if err != nil {
-	// 	return err
-	// }
+	if err != nil {
+		return err
+	}
 
 	return nil
 }
